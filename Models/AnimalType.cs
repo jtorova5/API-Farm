c37a5f6e5373311c3092a3dc59057c4e77299b7e
jtorova5 <jtorova5@gmail.com>
2024-09-17 07:06:21 -0500

    feat: AnimalType was configured for its later creation in the DB

--- Models/AnimalType.cs
+++ Models/AnimalType.cs
@@ -1,13 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace API_Farm.Models;
 
+[Table("animal_types")]
 public class AnimalType
 {
+    [Column("id")]
     public int Id { get; set; }
+
+    [Column("name")]
     public required string Name { get; set; }
+
+    [Column("description")]
     public string? Description { get; set; }
 }
